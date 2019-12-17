423800d253192b3228de2f1b351254a2189fdac0
Henok Gebremenfes <ggebremenfes@mum.edu>
2019-12-17 16:39:17 -0500

    today's change ticket number 100

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,15 +1,34 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.domain.Staff;
+import com.example.demo.service.StaffServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Hello")
 public class HelloController {
+      @Autowired
+      private StaffServices staffServices;
 
         @RequestMapping("/world")
         public String GetHello(){
             return "Hello world";
+        }
+
+        @RequestMapping("/{age}")
+        public List<Staff> getStaff(@PathVariable("age") int age ){
+            List<Staff> staffs = staffServices.createStaffList(age);
+            return staffs;
+        }
+
+        @RequestMapping("/{age}/{name}")
+        public List<Staff> getStaffs(@PathVariable("age") int age, @PathVariable("name")String name ){
+
+            List<Staff> staffs = staffServices.createStaffListTwo(age,name);
+            return staffs;
         }
     }
 
